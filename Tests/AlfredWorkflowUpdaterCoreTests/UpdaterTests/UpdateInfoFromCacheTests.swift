30e085798b551c2f1018f500e0c82985d1da80b0
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-24 14:37:16 +0800

    add UT for reading UpdateInfo from local cache file

--- Tests/AlfredWorkflowUpdaterCoreTests/UpdaterTests/UpdateInfoFromCacheTests.swift
+++ Tests/AlfredWorkflowUpdaterCoreTests/UpdaterTests/UpdateInfoFromCacheTests.swift
@@ -1,8 +1,27 @@
-//
-//  File.swift
-//  
-//
-//  Created by Guillaume Leclerc on 24/07/2022.
-//
+import XCTest
+@testable import AlfredWorkflowUpdaterCore
 
-import Foundation
+
+class UpdateInfoFromCacheTests: AlfredWorkflowUpdaterTestCase {
+
+    func test_that_if_there_is_no_updateInfoCacheFile_then_it_returns_nil() {
+        XCTAssertNil(
+            Updater.updateInfoFromCache()
+        )
+    }
+    
+    func test_that_if_there_is_an_updateInfoCacheFile_then_it_returns_the_correct_info() {
+        Self.mockAlreadyCreatedUpdateInfoFile(with: UpdateInfo(
+            version: "1.3.37",
+            file: "https://github.com/godbout/AlfredDummy/releases/download/1.3.37/AlfredDummy.alfredworkflow",
+            page: "https://github.com/godbout/AlfredDummy/releases/latest"
+        ))
+        
+        let updateInfo = Updater.updateInfoFromCache()
+        
+        XCTAssertEqual(updateInfo?.version, "1.3.37")
+        XCTAssertEqual(updateInfo?.file, "https://github.com/godbout/AlfredDummy/releases/download/1.3.37/AlfredDummy.alfredworkflow")
+        XCTAssertEqual(updateInfo?.page, "https://github.com/godbout/AlfredDummy/releases/latest")
+    }
+    
+}
